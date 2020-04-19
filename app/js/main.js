d579ebaffba4a0d9c015dd5eea7939ae26cbe05a
web-coder-maksim <clankedmax@gmail.com>
2020-04-19 09:22:34 +0300

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,15 @@ $(function(){
         $('.icon-th-list').removeClass('active');
     });
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+	    $(this).addClass('active');
+	    $('#'+id).addClass('active-tab').fadeIn();
+	    return false;
+    });
+
     $('.header__btn-menu').on('click', function(){
         $('.header__box').toggleClass('active');
     });
